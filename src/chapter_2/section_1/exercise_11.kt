a1733d8b8c818df3cf4e9639dba60ac443bd5469
Oskar Michalak <oskar.michalak@tapptic.com>
2022-01-16 11:34:09 +0100

    Add chapter 2 | section 1 | exercise 11

---
+++ src/chapter_2/section_1/exercise_11.kt
@@ -0,0 +1,39 @@
+package chapter_2.section_1
+
+object PrecalculatedSequenceShellSort {
+
+    private val H_SEQUENCE = intArrayOf(
+            1, 4, 13, 40, 121, 364, 1093, 3280, 9841, 29524, 88573, 265720, 797161, 2391484,
+            7174453, 21523360, 64570081, 193710244, 581130733
+    )
+    private val MAX_INPUT_SIZE = 3 * H_SEQUENCE[H_SEQUENCE.size - 1] + 1
+
+    fun sort(items: Array<Comparable<Any>>) {
+        val size = items.size
+        if (size <= 1) return
+        require(size <= MAX_INPUT_SIZE) { "Too big input array. Max array size is: $MAX_INPUT_SIZE" }
+
+        for (t in getMaxHIndex(size) downTo 0) {
+            val h = H_SEQUENCE[t]
+            for (i in h until size) {
+                var j = i
+                while (j >= h && less(items[j], items[j - h])) {
+                    exchange(items, j, j - h)
+                    j -= h
+                }
+            }
+        }
+    }
+
+    private fun getMaxHIndex(arraySize: Int) = H_SEQUENCE.findLast {
+        it < arraySize - 1
+    } ?: 0
+
+    private fun less(a: Comparable<Any>, b: Comparable<Any>) = a < b
+
+    private fun exchange(array: Array<Comparable<Any>>, aIndex: Int, bIndex: Int) {
+        val temp = array[aIndex]
+        array[aIndex] = array[bIndex]
+        array[bIndex] = temp
+    }
+}
